68de97c9716c56197eba55c98ab652895c56d05f
bryan <bojorquebryan@gmail.com>
2014-12-07 00:51:14 +0800

    fix memory leak on upload

--- server.js
+++ server.js
@@ -8,17 +8,7 @@ function startHttpServer(routerUrl, handle){
 		var pathname = urlObj.parse(request.url).pathname;
 		console.log("Request for "+ pathname +" received");
 
-		request.setEncoding("utf8");
-
-		request.addListener("data", function(postDataChunk){
-			postData += postDataChunk;
-			console.log("Received POST data chunk '"+postDataChunk+"' .");
-
-		});
-
-		request.addListener("end", function(){
-			routerUrl(handle,pathname,response, request);
-		});
+		routerUrl(handle,pathname,response, request);
 	}
 
 	httpObj.createServer(onRequest).listen(8888);
